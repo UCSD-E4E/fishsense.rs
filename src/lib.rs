f332c208033a4cdcc8bc5cb7924ffefea029d61d
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-05-27 23:20:01 -0700

    fix: remvoe fish module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 mod fish_segmentation;
 mod fish_head_tail_detector;
 
-pub mod fish {
-    pub use crate::fish_segmentation::{FishSegmentation, SegmentationError};
-    pub use crate::fish_head_tail_detector::FishHeadTailDetector;
-}
+pub use fish_segmentation::{FishSegmentation, SegmentationError};
+pub use fish_head_tail_detector::FishHeadTailDetector;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
