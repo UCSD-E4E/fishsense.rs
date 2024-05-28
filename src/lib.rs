e5e8695c3c4021bb7ef8a548510053ec781ed8d7
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-05-27 23:32:28 -0700

    feat: additional work on moving modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
-mod fish_segmentation;
-mod fish_head_tail_detector;
-
-pub mod fish {
-    pub use crate::fish_segmentation::{FishSegmentation, SegmentationError};
-    pub use crate::fish_head_tail_detector::FishHeadTailDetector;
-}
+pub mod fish;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
