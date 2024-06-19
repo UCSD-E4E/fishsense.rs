5679ce3608a16666a73386a13f3c90d5843b7519
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-06-19 12:46:54 -0700

    fix: make HeadTailError pub.

--- src/fish/mod.rs
+++ src/fish/mod.rs
@@ -2,4 +2,4 @@ mod fish_segmentation;
 mod fish_head_tail_detector;
 
 pub use fish_segmentation::{FishSegmentation, SegmentationError};
-pub use fish_head_tail_detector::FishHeadTailDetector;
+pub use fish_head_tail_detector::{FishHeadTailDetector, HeadTailError};
