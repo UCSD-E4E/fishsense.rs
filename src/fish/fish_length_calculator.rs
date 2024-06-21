7c3c23e98930136ff2277562afadf3ad2d494788
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-06-21 16:33:52 -0700

    fix: correct left and right step

--- src/fish/fish_length_calculator.rs
+++ src/fish/fish_length_calculator.rs
@@ -31,11 +31,11 @@ impl FishLengthCalculator {
         let mut right_direction = (&left_coord - &right_coord).mapv(|v| v as f32);
         right_direction /= norm(&right_direction);
 
-        let left_step = (&left_direction / array![left_direction[0], left_direction[1]]).mapv(|v| v as usize);
-        let right_step = (&right_direction / array![right_direction[0], right_direction[1]]).mapv(|v| v as usize);
+        let left_step = (left_coord.mapv(|v| v as f32) + &left_direction / array![left_direction[0], left_direction[1]]).mapv(|v| v as usize);
+        let right_step = (right_coord.mapv(|v| v as f32) + &right_direction / array![right_direction[0], right_direction[1]]).mapv(|v| v as usize);
 
-        println!("RUST: {} left depth: {}, step depth: {}", left_step, depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[left_step[0], left_step[1]]]);
-        println!("RUST: {} right depth: {}, step depth: {}", right_step, depth_mask[[right_coord[0], right_coord[1]]], depth_mask[[right_step[0], right_step[1]]]);
+        println!("RUST: {} {} left depth: {}, step depth: {}", left_coord, left_step, depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[left_step[0], left_step[1]]]);
+        println!("RUST: {} {} right depth: {}, step depth: {}", right_coord, right_step, depth_mask[[right_coord[0], right_coord[1]]], depth_mask[[right_step[0], right_step[1]]]);
 
         (depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[right_coord[0], right_coord[1]]])
     }
