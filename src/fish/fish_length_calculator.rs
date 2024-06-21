27c76032c58172ae11235bf05888ae6d5aed621a
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-06-21 16:29:31 -0700

    chore: add additional logging

--- src/fish/fish_length_calculator.rs
+++ src/fish/fish_length_calculator.rs
@@ -34,8 +34,8 @@ impl FishLengthCalculator {
         let left_step = (&left_direction / array![left_direction[0], left_direction[1]]).mapv(|v| v as usize);
         let right_step = (&right_direction / array![right_direction[0], right_direction[1]]).mapv(|v| v as usize);
 
-        println!("RUST: left depth: {}, step depth: {}", depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[left_step[0], left_step[1]]]);
-        println!("RUST: right depth: {}, step depth: {}", depth_mask[[right_coord[0], right_coord[1]]], depth_mask[[right_step[0], right_step[1]]]);
+        println!("RUST: {} left depth: {}, step depth: {}", left_step, depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[left_step[0], left_step[1]]]);
+        println!("RUST: {} right depth: {}, step depth: {}", right_step, depth_mask[[right_coord[0], right_coord[1]]], depth_mask[[right_step[0], right_step[1]]]);
 
         (depth_mask[[left_coord[0], left_coord[1]]], depth_mask[[right_coord[0], right_coord[1]]])
     }
